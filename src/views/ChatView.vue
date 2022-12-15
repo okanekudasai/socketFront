2eecabebc76c7ae980a3a6c85f9084ad4143feb1
opi6opi6 <opi6@hanmail.net>
2022-12-15 13:26:04 +0900

    second

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,31 +1,91 @@
 <template>
     <div>
-        방이름:{{this.$route.params.roomName}}<br>
-        참가자:
+        {{this.$store.state.subscribeList}}<br>
+        방이름 : {{this.$route.params.roomName}}<br>
+        참가자 :
+        <span v-for="(user, idx) in userList" :key="idx">
+            {{user}}
+        </span>
         <hr>
-        <input>
-        <button>전송</button>
+        <input v-model="message" />
+        <button @click="submitMessage">전송</button>
+        <hr>
         <div>
-
-            여기에 채팅내용이 나타나요
-
+            <div v-for="(m, idx) in messageList" :key="idx">
+                {{m.nickname}} :: {{m.message}} / {{m.date.getHours()}}시 {{m.date.getMinutes()}}분 {{m.date.getSeconds()}}초
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
 export default {
     data() {
         return {
-            userList: []
+            userList: [],
+            message: "",
+            messageList: []
         }
     },
     created() {
         this.start();
     },
     methods: {
+        //시작할 땐 stompClient가 있는지 없는지 검사해봐요 새로고침하면 stompClient가 없어지니까요
         start() {
-            
+            //stompClient가 없다면 먼저 stompClient객체를 만들어 줘요
+            if (!this.$store.state.stompClient) {
+                const socket = new SockJS('http://localhost:9998/realsocket/websocket');
+                this.$store.state.stompClient = Stomp.over(socket);
+                this.$store.state.stompClient.connect(
+                    {},
+                    () => {
+                    console.log("야호 연결성공요")
+                    this.updateSubscribe();
+                });
+            }
+            //stompClient가 있다면 바로 subscribe를 갱신해요
+            else {
+                this.updateSubscribe();
+            }
+        },
+        subscribeCancel() {
+            for(let sub of this.$store.state.subscribeList) {
+                this.$store.state.stompClient.unsubscribe(sub.id);
+            }
+            this.$store.state.subscribeList = [];
+        },
+        updateSubscribe() {
+            console.log(sessionStorage.getItem("roomNumber"))
+            axios.post("http://localhost:9998/realsocket/findRoom", sessionStorage.getItem("roomNumber"), { headers: {'Content-Type': 'text/plain'}}).then(({data}) => {
+                this.userList = data.users;
+                // 방리스트에 관한 구독을 끊어요
+                this.subscribeCancel()
+                // 들어간 채팅방에 대한 구독을 해요
+                // 첫 번째 구독은 메세지에 대한 구독이에요
+                console.log("data.roomNumber : ", data.roomNumber)
+                const messageSubscribeId = this.$store.state.stompClient.subscribe("/topic/message/" + data.roomNumber, (e) => {
+                    //메세지를 받았어요 메세지는 e.body에 있고 {message: ~~~, nickname: ~~, date: ~~~}에요
+                    var rawData = JSON.parse(e.body);
+                    rawData.date = new Date(rawData.date);
+                    this.messageList.push(rawData);
+                })
+                this.$store.state.subscribeList.push(messageSubscribeId);
+                // 두 번째 구독은 채팅방 입장 퇴장에 대한 구독이에요
+                const NoteSubscribeId = this.$store.state.stompClient.subscribe("/topic/notification/" + data.roomNumber)
+                this.$store.state.subscribeList.push(NoteSubscribeId);
+            });
+        },
+        submitMessage() {
+            // 전달할 값은 {아이디: 내아이디, 메세지: this.message}에요
+            const data = {
+                message : this.message,
+                nickname : this.$route.params.userId,
+            }
+            this.$store.state.stompClient.send("/socket/sendMessage/" + sessionStorage.getItem("roomNumber"), JSON.stringify(data), {})
         }
     },
     
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
+        {{this.$store.state.subscribeList}}<br>
         채팅방 목록
         <button @click="makeRoom">방 만들기</button>
+        <button @click="subscribeCancel">임시버튼</button>
         <hr>
         <div v-if="roomList.length == 0">방없음</div>
         <div v-else>
-            <div v-for="(room, idx) in roomList" :key="idx">
+            <div v-for="(room, idx) in roomList" :key="idx" @click="enterChat(room)">
                 {{room.roomName}}
             </div>
         </div>
@@ -34,10 +36,17 @@ export default {
                 {},
                 () => {
                 console.log("야호 연결성공요")
-                this.$store.state.stompClient.subscribe("/topic/roomList", () => {
+                // 모든 구독을 끊어요
+                this.subscribeCancel()
+                const subscribeId = this.$store.state.stompClient.subscribe("/topic/roomList", (e) => {
+                    console.log(e.body)
+                    //이 함수를 써서 vue객체의 룸리스트를 업데이트해요
+                    console.log("방리스트를 업데이트 할게요")
                     this.updateRoomList();
                     //e로 던저서 e.body로 받았어요
                 })
+                // 구독 정보를 업데이트해요
+                this.$store.state.subscribeList.push(subscribeId);
                 this.updateRoomList();
             });
         },
@@ -46,20 +55,38 @@ export default {
                 this.roomList = data;
             })
         },
+        subscribeCancel() {
+            for(let sub of this.$store.state.subscribeList) {
+                this.$store.state.stompClient.unsubscribe(sub.id);
+            }
+            this.$store.state.subscribeList = [];
+        },
         makeRoom() {
             var roomName = prompt("방제목을 입력해주세요");
-            if (roomName == null) return;
+            if (!roomName) return;
             var userId = prompt("닉네임을 입력해주세요");
-            if (userId == null) return;
+            if (!userId) return;
             console.log(roomName, userId)
+            //채팅방이름과 내 닉네임을 가지고 채팅방을 만드는 메서드를 호출해요
             axios.post("http://localhost:9998/realsocket/chatingRoom", {"roomName": roomName, "userId": userId}).then(({data}) => {
                 console.log(data)
                 sessionStorage.setItem("roomNumber", data)
                 // 내가 방을 만든 사실을 구독자들에게 알려줘요
-                this.$store.state.stompClient.send("/topic/roomList")
+                this.$store.state.stompClient.send("/socket/roomList")
+                // 채팅방으로 이동해요
                 this.$router.push( {path: '/ChatView/' + roomName + "/" + userId} )
             })
         },
+        enterChat(room) {
+            console.log(room)
+            var userId = prompt("닉네임을 입력해주세요");
+            if (!userId) return;
+            axios.post("http://localhost:9998/realsocket/enterChat", {"roomNumber": room.roomNumber, "userId": userId}).then(({data}) => {
+                sessionStorage.setItem("roomNumber", room.roomNumber);
+                this.$store.state.stompClient.send("/socket/sendMessage/" + sessionStorage.getItem("roomNumber"), JSON.stringify(data), {})
+                this.$router.push( {path: '/ChatView/' + data + "/" + userId} )
+            })
+        }
     },
 };
 </script>
