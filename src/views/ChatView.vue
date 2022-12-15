189637e9e8cbeb6bf5abfd8b078efd10bb9e02dd
opi6opi6 <opi6@hanmail.net>
2022-12-15 09:04:13 +0900

    first

---
+++ src/views/ChatView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        방이름:{{this.$route.params.roomName}}<br>
+        참가자:
+        <hr>
+        <input>
+        <button>전송</button>
+        <div>
+
+            여기에 채팅내용이 나타나요
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userList: []
+        }
+    },
+    created() {
+        this.start();
+    },
+    methods: {
+        start() {
+            
+        }
+    },
+    
+};
+</script>
+
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        채팅방 목록
+        <button @click="makeRoom">방 만들기</button>
+        <hr>
+        <div v-if="roomList.length == 0">방없음</div>
+        <div v-else>
+            <div v-for="(room, idx) in roomList" :key="idx">
+                {{room.roomName}}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            roomList: [],
+        }
+    },
+    created() {
+        this.start();
+    },
+    methods: {
+        start() {
+            const socket = new SockJS('http://localhost:9998/realsocket/websocket');
+            this.$store.state.stompClient = Stomp.over(socket);
+            this.$store.state.stompClient.connect(
+                {},
+                () => {
+                console.log("야호 연결성공요")
+                this.$store.state.stompClient.subscribe("/topic/roomList", () => {
+                    this.updateRoomList();
+                    //e로 던저서 e.body로 받았어요
+                })
+                this.updateRoomList();
+            });
+        },
+        updateRoomList() {
+            axios.get("http://localhost:9998/realsocket/chatingRoomList").then(({data}) => {
+                this.roomList = data;
+            })
+        },
+        makeRoom() {
+            var roomName = prompt("방제목을 입력해주세요");
+            if (roomName == null) return;
+            var userId = prompt("닉네임을 입력해주세요");
+            if (userId == null) return;
+            console.log(roomName, userId)
+            axios.post("http://localhost:9998/realsocket/chatingRoom", {"roomName": roomName, "userId": userId}).then(({data}) => {
+                console.log(data)
+                sessionStorage.setItem("roomNumber", data)
+                // 내가 방을 만든 사실을 구독자들에게 알려줘요
+                this.$store.state.stompClient.send("/topic/roomList")
+                this.$router.push( {path: '/ChatView/' + roomName + "/" + userId} )
+            })
+        },
+    },
+};
 </script>
+
+<style>
+</style>
+
